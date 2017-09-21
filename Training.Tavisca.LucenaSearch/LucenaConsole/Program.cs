85456404198f96fa43fc76e882724b9d95773304
Suraj Narayan <snarayan@tavisca.com>
2017-09-21 20:55:50 +0530

    Added the Lucene File

---
+++ Training.Tavisca.LucenaSearch/LucenaConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training.Tavisca.LucenaSearch;
+
+
+namespace LucenaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<SampleInformation> HotelList = SampleDataSearch.GetAll();
+            LuceneSearch.AddUpdateLuceneIndex(HotelList);
+            Console.WriteLine("WelCome to the world of lucena");
+            while(true)
+            {
+                Console.WriteLine("Enter the Search Query");
+                string querry = Console.ReadLine();
+                IEnumerable<SampleInformation> HotelCount = LuceneSearch._search(querry);
+                foreach (SampleInformation hotelInfo in HotelCount)
+                {
+                  
+                    Console.WriteLine($"The Hotel Id-> :{hotelInfo.id}The Hotel Type:{hotelInfo.type}Hotel-Description:{hotelInfo.Description}");
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Training.Tavisca.LucenaSearch/Training.Tavisca.LucenaSearch/SampleDataSearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.Tavisca.LucenaSearch
+{
+    public class SampleDataSearch
+    {
+        public static SampleInformation Get(int id)
+        {
+            return GetAll().SingleOrDefault(x => x.id.Equals(id));
+        }
+        public static List<SampleInformation> GetAll()
+        {
+            return new List<SampleInformation> {
+        new SampleInformation {id = 123,type="Hotel ", Description = "Oyo rooms  hotel present in viman nagar in pune"},
+        new SampleInformation {id=345 ,type="Hotel", Description = "One of the most  reputed hotel  in pune"},
+        new SampleInformation { id =678, type="Hotel" ,Description = "Five star Hotel in jamshedpur"},
+        new SampleInformation {id=91011, type="Hotel" ,Description = "Hotel in Chicago" },
+        new SampleInformation {id=123,  type="CabService" ,Description = "City in USA"}
+    };
+        }
+    }
+}   
+
